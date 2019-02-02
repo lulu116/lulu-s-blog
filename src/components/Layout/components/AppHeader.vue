d9a558204fa940c2793f4d5c89ce75f11ae05d37
lulu116 <sliusting970@163.com>
2019-02-02 17:57:29 +0800

    优化生产环境

--- src/components/Layout/components/AppHeader.vue
+++ src/components/Layout/components/AppHeader.vue
@@ -7,22 +7,35 @@
       :is-Collapse="isCollapse"
       :hamburgerIcon="isCollapse ? 'zhankai_right' : 'zhankai_left'"
       class="hamburger-container"
+      @click="handleClickChangeHamburger"
     />
     <bread-crumb/>
-    <el-dropdown trigger="click" class="user-dropdown">
+    <el-dropdown trigger="click" class="user-dropdown" @command="handleCommand">
       <span>
         <img src="@/assets/big.png"/>
         {{userName}}<i class="el-icon-caret-bottom el-icon--right"></i>
       </span>
       <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item class="clearfix" command="a">
-          修改密码
-        </el-dropdown-item>
         <el-dropdown-item class="clearfix" command="b">
           退出
         </el-dropdown-item>
       </el-dropdown-menu>
     </el-dropdown>
+   <!-- <el-tooltip class="item" effect="light" content="segmentfault">
+      <a href="https://segmentfault.com/u/lulu116/notes" target="_blank" class="github-css">
+        <img src="@/assets/sf.png" alt="segmentfault" class="github-svg sf" />
+      </a>
+    </el-tooltip>-->
+    <el-tooltip class="item" effect="light" content="我的邮箱">
+      <a href="https://github.com/lulu116" target="_blank" class="github-css">
+        <svg-icon iconClass="email" className="github-svg"></svg-icon>
+      </a>
+    </el-tooltip>
+    <el-tooltip class="item" effect="light" content="我的github">
+      <a href="https://github.com/lulu116" target="_blank" class="github-css">
+        <svg-icon iconClass="github" className="github-svg"></svg-icon>
+      </a>
+    </el-tooltip>
   </el-menu>
 </template>
 
@@ -43,7 +56,16 @@ export default {
   },
   methods: {
     handleClickChangeHamburger () {
+      debugger
       return !this.isCollapse
+    },
+    handleCommand (command) {
+      if (command === 'b') {
+        this.logout()
+      }
+    },
+    logout () {
+      alert('确认要退出吗？')
     }
   }
 }
@@ -56,10 +78,12 @@ export default {
     .user-dropdown {
       float: right;
       margin-right: 15px;
+      cursor: pointer;
       span {
         height: 100%;
         display: inline-block;
         img {
+          vertical-align: middle;
           width: 30px;
           height: 30px;
           border-radius: 50%;
@@ -67,5 +91,24 @@ export default {
         }
       }
     }
+    .github-css {
+      float: right;
+      width: 50px;
+      display: inline-block;
+      height: 60px;
+      margin-right: 10px;
+      text-align: center;
+    }
+    .github-css:hover .github-svg{
+      color: #606266f5;
+    }
+  }
+  .github-svg {
+    font-size: 30px;
+    color: #999999ed;
+  }
+  .sf {
+    vertical-align: middle;
+    border-radius: 50%;
   }
 </style>
